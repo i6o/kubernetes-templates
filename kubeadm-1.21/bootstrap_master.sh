68af3ef8c7a2f7d4695cd8d92581817d3557fc30
Marko Ikäheimo <mikaheim@gmail.com>
2021-05-13 15:07:47 +0300

    Fix kubeconfig fetch

--- kubeadm-1.21/bootstrap_master.sh
+++ kubeadm-1.21/bootstrap_master.sh
@@ -7,5 +7,9 @@ sudo kubeadm config images pull
 
 sudo kubeadm init --pod-network-cidr=${POD_NETWORK_CIDR} --control-plane-endpoint "${APISERVER_NAME}:${APISERVER_DEST_PORT}" --upload-certs
 
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 # Install CNI - Flannel - change this to CNI of your choice.
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
