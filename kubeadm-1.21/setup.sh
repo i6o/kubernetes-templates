84ab10ab14ac279f693265f9e942754fd68177b9
Marko Ikäheimo <mikaheim@gmail.com>
2021-05-16 09:54:44 +0300

    Added backup_etcd.sh

--- kubeadm-1.21/setup.sh
+++ kubeadm-1.21/setup.sh
@@ -15,9 +15,12 @@ chmod a+x ./bootstrap_master.sh
 wget https://raw.githubusercontent.com/i6o/kubernetes-templates/main/kubeadm-1.21/setup_lb.sh
 chmod a+x ./setup_lb.sh
 
+wget https://raw.githubusercontent.com/i6o/kubernetes-templates/main/kubeadm-1.21/backup_etcd.sh
+chmod a+x ./backup_etcd.sh
+
 wget https://raw.githubusercontent.com/i6o/kubernetes-templates/main/kubeadm-1.21/hosts
 wget https://raw.githubusercontent.com/i6o/kubernetes-templates/main/kubeadm-1.21/env-variables
 
 popd
 
-echo All files at ~/k8s-bootstrap. Edit hosts and env-variables files atleast
+echo All files at ~/k8s-bootstrap. Edit hosts and env-variables files atleast ! ! !
